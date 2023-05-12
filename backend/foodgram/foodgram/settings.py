c47164cb626ecf9f78d51146af02023e9799f0d2
Artem Nosov <nosov1995@gmail.com>
2023-05-12 17:43:52 +0200

    fix media-url

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -129,5 +129,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = f'http://{os.getenv("HOST")}/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
